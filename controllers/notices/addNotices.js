ab8b8728a48c3dff29383b166baf79ad01de11cc
Oleksandr <alexandrokrk@gmail.com>
2022-11-24 23:08:18 +0200

    1

--- controllers/notices/addNotices.js
+++ controllers/notices/addNotices.js
@@ -1,19 +1,19 @@
-const { Notices } = require("../../models/schemasNotices");
-// const { RequestError } = require("../../helpers");
+// const { Notices } = require("../../models/schemasNotices");
+// // const { RequestError } = require("../../helpers");
 
-const addNotices = async (req, res) => {
-  const { body } = req;
-  const { _id: owner } = req.user;
-  const results = await Notices.create({
-    ...body,
-    owner,
-  });
-  res.json({
-    status: "success",
-    code: 200,
-    message: "Notice crested",
-    data: { notice: results },
-  });
-};
+// const addNotices = async (req, res) => {
+//   const { body } = req;
+//   const { _id: owner } = req.user;
+//   const results = await Notices.create({
+//     ...body,
+//     owner,
+//   });
+//   res.json({
+//     status: "success",
+//     code: 200,
+//     message: "Notice crested",
+//     data: { notice: results },
+//   });
+// };
 
-module.exports = addNotices;
+// module.exports = addNotices;
