0990327da1dd4b7c1ba9819591024846ae4865be
Oleksandr <alexandrokrk@gmail.com>
2022-11-27 17:00:17 +0200

    1

--- middleware/multer.js
+++ middleware/multer.js
@@ -4,7 +4,7 @@ const tempDir = path.join(__dirname, "../", "tmp");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./uploads");
+    cb(null, tempDir);
   },
   filename: function (req, file, cb) {
     cb(null, new Date().toISOString() + "-" + file.originalname);
