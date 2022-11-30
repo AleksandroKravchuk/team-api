0aa49d3a9cb7dd2aea233405cba317147e79dfcb
Oleksandr <alexandrokrk@gmail.com>
2022-11-30 12:40:12 +0200

    1

--- routes/api/notices.js
+++ routes/api/notices.js
@@ -19,7 +19,7 @@ router.get("/find/favorite", authenticate, ctrlWrapper(ctrl.getFavoriteNotice));
 router.post(
   "/",
   authenticate,
-  uploader.single("photoPet"),
+  upload.single("photoPet"),
   // upload.single("photoNotices"),
   schemasNotice.noticeAddValidation,
   ctrlWrapper(ctrl.createNoticeCloud)
