b7e318e97c927562ab37217652179f5784ef53a1
Oleksandr <alexandrokrk@gmail.com>
2022-11-30 14:59:00 +0200

    1

--- controllers/notices/createNoticeCloud.js
+++ controllers/notices/createNoticeCloud.js
@@ -1,4 +1,4 @@
-const fs = require("fs");
+const fs = require("fs/promises");
 const path = require("path");
 const { Notices } = require("../../models/schemasNotices");
 const { configImg, RequestError } = require("../../helpers");
