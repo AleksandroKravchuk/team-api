fb2f9ee27d76c92a7ff78f8c6d6798fec0d07681
Oleksandr <alexandrokrk@gmail.com>
2022-11-30 12:55:52 +0200

    1

--- controllers/pets/index.js
+++ controllers/pets/index.js
@@ -1,9 +1,11 @@
 const getAllPets = require("./getAllPets");
 const deletePet = require("./deletePet");
 const addPetInfo = require("./addPetInfo");
+const addPetInfoCloud = require("./addPetInfoCloud");
 
 module.exports = {
   getAllPets,
   deletePet,
   addPetInfo,
+  addPetInfoCloud,
 };
