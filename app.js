67ec3d0b042acf59c9eb49c7ef76bd897c8d0265
Oleksandr <alexandrokrk@gmail.com>
2022-12-16 13:35:43 +0200

    1

--- app.js
+++ app.js
@@ -18,16 +18,12 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 // app.use(bodyParser.json);
 app.use(logger(formatsLogger));
 app.use(cors());
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
   );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
   next();
 });
 app.use(express.json());
